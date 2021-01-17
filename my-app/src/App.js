428b9608f071db6dc81c7c0326d05e52adb10f3e
rvaladez3 <rvaladez3@ucmerced.edu>
2021-01-17 10:48:51 -0800

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,42 @@
+
 import logo from './logo.svg';
 import './App.css';
+import landing from './pages/Landing/landing';
+import sign_in from './pages/Landing/sign_in';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParamsE
+} from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+        <Router>
+          
+           <div className="App">
+           <landing/>
+            {/* <Route path = "/" component={Home}/> */}
+            {/* <Route path = "/record" component={Record}/> */}
+            <Route path = "/landing" component={landing}/>
+            <Route path = "/sign_in" component={sign_in}/>
+           </div>
+           
+           </Router>
+
+      
   );
 }
+
+const Home = () => (
+  <div>
+    <h1> Home Page </h1>
+  </div>
+);
 
 export default App;
---
+++ my-app/src/pages/Landing/landing.js
@@ -0,0 +1,40 @@
+
+import './landing.css';
+
+import like from './svg/exercise.svg';
+import logo from './svg/logo.png';
+
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+import { positions } from '@material-ui/system';
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    useRouteMatch,
+    useParams
+  } from "react-router-dom";
+
+
+function landing() {
+  return (
+    <div className="App">
+    
+      <h2>
+      <Button variant="contained" color="primary" href="#contained-buttons" right = "50px">  
+  Login </Button>
+  </h2>
+  <img src={like} id = "like" alt = 'like' />
+  <img src={logo} id = "logo" alt = 'logo' />
+  <h3>
+  <Button variant="contained" color="primary"  href="#contained-buttons" >
+      Sign-up
+    </Button>
+    </h3>
+    </div>
+  );
+}
+
+export default landing;
