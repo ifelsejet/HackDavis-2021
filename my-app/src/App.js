1a72249d865e7cb3e39f8ff93165321c8f937fba
rvaladez3 <rvaladez3@ucmerced.edu>
2021-01-16 22:37:25 -0800

    update
    partial front end done for landing.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,40 @@
+
 import logo from './logo.svg';
 import './App.css';
+import landing from './pages/Landing/landing';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParamsE
+} from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+        <Router>
+          
+           <div className="App">
+           <landing/>
+            {/* <Route path = "/" component={Home}/> */}
+            {/* <Route path = "/record" component={Record}/> */}
+            <Route path = "/landing" component={landing}/>
+           </div>
+           
+           </Router>
+
+      
   );
 }
+
+const Home = () => (
+  <div>
+    <h1> Home Page </h1>
+  </div>
+);
 
 export default App;
